2c72b3da8312e56ad499e58f09eea727509c3c50
tboone <tboone@ncsu.edu>
2021-09-09 20:31:00 -0400

    Comments

--- demo/src/main/java/com/example/client.java
+++ demo/src/main/java/com/example/client.java
@@ -1,23 +1,13 @@
 package com.example;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-/**
- *
- * @author troy
- */
+ /**
+  * @author Troy Boone
+  * Socket Program. The client file will send a radius and height to receive the volume of the matching cylinder  
+  */
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 public class client {
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String args[]) {
         int port = 5000;
         String host = "127.0.0.1";
@@ -26,26 +16,30 @@ public class client {
         Socket socket;
         Scanner reader = new Scanner(System.in);
         System.out.println("Trying connection . . .");
-        try {            
+        try {
+            //Try and connect to server and streams
             socket = new Socket(host, port);
             System.out.println("Connected");
-
             in = new DataInputStream(socket.getInputStream());
             out = new DataOutputStream(socket.getOutputStream());
+            //Send details
             System.out.println("What is the radius?");
             out.writeDouble(reader.nextDouble());
             System.out.println("What is the height?");
             out.writeDouble(reader.nextDouble());
+            //Receive details
             System.out.println("The radius is " + in.readDouble());
             System.out.println("The height is " + in.readDouble());
             System.out.println("The volume is " + in.readDouble());
 
+            //Close all the streams
             socket.close();
             reader.close();
             in.close();
             out.close();
 
         } catch (Exception e) {
+            //Connection Failed
             System.out.println(e);
         }
 
--- demo/src/main/java/com/example/server.java
+++ demo/src/main/java/com/example/server.java
@@ -6,16 +6,12 @@
 package com.example;
 
 /**
- *
- * @author troy
+ * @author Troy
+ * Socket Program. The client file will send a radius and height to receive the volume of the matching cylinder  
  */
 import java.io.*;
 import java.net.*;
 public class server {
-  
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String args[]) {
         int port = 5000;
         DataInputStream in;
@@ -25,24 +21,26 @@ public class server {
         try {
             server = new ServerSocket(port);
             System.out.println("Server started");
-
             System.out.println("Waiting for a client ...");
-
+            // Client and streams connected
             socket = server.accept();
             System.out.println("Client accepted");
             in = new DataInputStream(socket.getInputStream());
             out = new DataOutputStream(socket.getOutputStream());
+            //Assign variables 
             double radius = in.readDouble();
             double height = in.readDouble();
             double volume = Math.PI * radius * radius * height;
+            //Return Variables
             out.writeDouble(radius);
             out.writeDouble(height);
             out.writeDouble(volume);
-
+            //Close streams
             socket.close();
             in.close();
             out.close();
         } catch (Exception e) {
+            //Socket Failed
             System.out.println(e);
         }
 
